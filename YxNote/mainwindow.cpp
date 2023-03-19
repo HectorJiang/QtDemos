8040c5ddd2a73d357328264ecaceae5ab1bee160
hectorjiang <jiangyongxu1208@gmail.com>
2023-03-19 20:10:03 +0800

    create yxnote

---
+++ YxNote/mainwindow.cpp
@@ -0,0 +1,25 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowFlags(Qt::FramelessWindowHint);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_m_pSliderOpacity_valueChanged(int value)
+{
+    qreal res = static_cast<qreal>(value)/10;
+    qDebug()<<res;
+    setWindowOpacity(res);
+}
